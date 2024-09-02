f8d41958cbdcee57153acee13cdafaa88901608e
Kervis <kervisramon@gmail.com>
2024-09-02 16:28:34 -0400

    Vite cambio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/paletas_vivax/',  // Reemplaza 'paletas_vivax' con el nombre de tu repositorio
   plugins: [react()],
 })
